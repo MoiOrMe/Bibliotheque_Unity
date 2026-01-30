18490a91bb69b7b845332854a6ab67c3ba894d3d
MoiOrMe <robin.rivard@efrei.net>
2026-01-30 18:45:04 +0100

    (UI) PauseMenu + ConfirmPanel

--- Assets/_MyLibraries/_Core/Input/InputMapSwitcher.cs
+++ Assets/_MyLibraries/_Core/Input/InputMapSwitcher.cs
@@ -1,46 +1,59 @@
 using UnityEngine;
 
-// Composant utilitaire permettant de changer la Map d'Input active (Gameplay vs UI)
-// via des appels de méthode publics. Utile pour être appelé par des UnityEvents
-// dans l'inspecteur ou par le GameManager lors des changements d'état.
-
 namespace MyLib.Core.Input
 {
+    /// <summary>
+    /// Composant utilitaire permettant de changer la Map d'Input active
+    /// </summary>
     public class InputMapSwitcher : MonoBehaviour
     {
+        #region Internal State
         [Header("Dependencies")]
         [Tooltip("Référence vers le ScriptableObject InputReader partagé.")]
         [SerializeField] private InputReader _inputReader;
 
         [Header("Settings")]
         [Tooltip("Si vrai, active la map Gameplay au démarrage de ce composant.")]
-        [SerializeField] private bool _enableGameplayOnStart = true;
+        [SerializeField] private bool _enableUIyOnStart = true;
+        #endregion
 
-        // Configuration initiale lors du démarrage.
+        #region Unity Life Cycle
+        /// <summary>
+        /// Configuration initiale lors du démarrage.
+        /// </summary>
         private void Start()
         {
-            if (_inputReader != null && _enableGameplayOnStart)
+            if (_inputReader != null && _enableUIyOnStart)
             {
-                _inputReader.EnableGameplayInput();
+                _inputReader.EnableUIInput();
             }
         }
+        #endregion
 
-        // Méthode publique pour activer les contrôles de jeu (Déplacements, Tirs).
+        #region Public Methods
+        /// <summary>
+        /// Active les contrôles de jeu (Déplacements, Tirs).
+        /// </summary>
         public void SwitchToGameplay()
         {
             _inputReader.EnableGameplayInput();
         }
 
-        // Méthode publique pour activer les contrôles d'interface (Navigation menus).
+        /// <summary>
+        /// Active les contrôles d'interface (Navigation menus).
+        /// </summary>
         public void SwitchToUI()
         {
             _inputReader.EnableUIInput();
         }
 
-        // Méthode publique pour désactiver tous les contrôles (ex: cinématiques).
+        /// <summary>
+        /// Désactive tous les contrôles (ex: cinématiques).
+        /// </summary>
         public void DisableAll()
         {
             _inputReader.DisableAllInput();
         }
+        #endregion
     }
 }
